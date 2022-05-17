3766dce587876880bfe13c8ab29fae9f3ac7fdf6
Carolline Santos <77252716+Carolsan@users.noreply.github.com>
2022-05-17 14:27:34 -0300

    Add files via upload

---
+++ jogo1/Game.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+var game_end = false
+var moves = 0
+
+func _process(delta):
+	$MovesLabel.text = 'Moves: ' + str(moves)
+	if game_end == false:
+		var spots = $Spots.get_child_count()
+		for i in $Spots.get_children():
+			if i.occupied:
+				spots -= 1
+			
+		if spots == 0:
+			$AcceptDialog.popup()
+			game_end = true
+			
+			
+func _on_AcceptDialog_confirmed():
+	get_tree().reload_current_scene()
